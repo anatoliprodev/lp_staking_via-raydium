f824b9a378805f70f0fff8b80bd557b75a393948
anatoliprodev <tolikmia@yandex.by>
2022-01-18 21:52:27 +0900

    project finished

---
+++ programs/stable-pool-faucet/src/states.rs
@@ -0,0 +1,11 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(Default)]
+pub struct Faucet {
+    pub super_owner: Pubkey,
+    pub mint_usdc_usdr_lp: Pubkey,
+    pub mint_eth_sol_lp: Pubkey,
+    pub mint_atlas_ray_lp: Pubkey,
+    pub mint_samo_ray_lp: Pubkey,
+}
